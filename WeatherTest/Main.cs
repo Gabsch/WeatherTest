76f4de8e3130fc72f0aed0031e4ebd945b23cc7c
Gabriel D. Schneider <gabriel.d.schneider@outlook.com>
2019-01-25 17:15:51 +0100

    Weather command implementation

---
+++ WeatherTest/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using GTANetworkAPI;
+
+namespace WeatherTest
+{
+    public class Main : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("WeatherTest resource loaded!");
+        }
+
+        [Command("weather")]
+        public void SetWeather(Client sender, string weatherId = "")
+        {
+            if (!String.IsNullOrEmpty(weatherId) && Enum.TryParse(typeof(Weather), weatherId, ignoreCase: true, out var weather))
+            {
+                NAPI.World.SetWeather((Weather)weather);
+                return;
+            }
+
+            NAPI.Chat.SendChatMessageToPlayer(sender, $"{weatherId} is not a valid value. Valid values are: {String.Join(", ", Enum.GetNames(typeof(Weather)))}");
+        }
+    }
+}
